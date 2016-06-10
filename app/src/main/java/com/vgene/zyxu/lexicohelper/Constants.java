2b51f84e985a52a003470f2202393c92f54fc975
Ziyang Xu <xuziyang@pku.edu.cn>
2016-06-10 14:02:06 +0800

    final GA update

---
+++ app/src/main/java/com/vgene/zyxu/lexicohelper/Constants.java
@@ -0,0 +1,21 @@
+package com.vgene.zyxu.lexicohelper;
+
+/**
+ * Created by zyxu on 5/31/16.
+ */
+public class Constants {
+    public static final double B_DEFAULT = 0.155;
+    public static final double MUTATING_RATE = 0.1;
+    public static final int POPULATION_SIZE = 20;
+    public static final int ITERATION_TIME = 15;
+    public static final int WORD_GROUP_COUNT = 7;
+
+    public static final int C1 = 10000;
+    public static final int C2 = 2;
+    public static final int C3 = 300;
+    public static final int C4 = 400;
+
+    public static final int WEIGHT_F1 = 1;
+    public static final int WEIGHT_F2 = 100;
+
+}
